a8d58b5de56d2e3ede620d56c36a3e86e5186691
Zachary <zturner29@gmail.com>
2015-09-18 13:51:14 -0500

    ajax assignment

--- examples.js
+++ examples.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
     $.ajax('data.json', {
         success: function(response){
             console.log(response);
+            
+            for(var it = 0; it < response.length; it++) {
+            	var myContent = $("<div style='background:" + response[it].value + "'><p>" + response[it].color + "</p></div>");
+            	$('.content-placeholder').append(myContent);
+            }
+            
         },
         error: function(request, errorType, errorMessage){
             alert(errorType + " " + errorMessage);
